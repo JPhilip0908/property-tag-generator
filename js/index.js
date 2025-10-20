41f327fe37d0cafd0d2b88a7164778ec460c442d
JPhilip0908 <johnphilip0908@yahoo.com>
2025-10-21 04:23:50 +0800

    add function to upload logo button, add additional behavior for select all checkbox

--- js/index.js
+++ js/index.js
@@ -4,13 +4,51 @@ import { defaultTable } from "./modules/table-object.js";
 
 let wbData = null;
 let table = null;
-const selectAllchkBox = $("#selectAll");
-
+const chkSelectAll = $("#selectAll");
+const btnLogo = $("#logoUpload");
+const dataTable = $("#dataTable");
+let logoDataURL = null;
+let logoPreview = $("#logoPreview");
 const columns = ["Property Number", "Asset Item", "Manufacturer", "Model", "Serial Number", "Cost of Acquisition", "Date of Acquisition", "Date Issued", "Name of Accountable Officer", "Asset Location", "Current Condition"];
 
-table = $("#dataTable").DataTable(defaultTable());
+table = dataTable.DataTable(defaultTable());
 
-selectAllchkBox.on("change", function () {
+//select all rows when checked
+chkSelectAll.on("change", function () {
   const checked = $(this).is(":checked");
   $("#dataTable tbody input.row-checkbox").prop("checked", checked).trigger("change");
+});
+
+//update selected count
+function updateSelectedCount() {
+  const count = $("#dataTable tbody input.row-checkbox:checked").length;
+  $("#selectedCount").text(count + " selected");
+}
+
+$("#dataTable tbody").on("change", "input.row-checkbox", function () {
+  updateSelectedCount();
+  //uncheck header select checkboxes if none were checked
+  const all = $("#dataTable tbody input.row-checkbox").length;
+  const checked = $("#dataTable tbody input.row-checkbox:checked").length;
+  $("#selectAll").prop("checked", all > 0 && all === checked);
+});
+
+btnLogo.on("change", function (event) {
+  const file = event.target.files && event.target.files[0];
+  if (!file) {
+    logoPreview.text("Logo");
+    return;
+  }
+
+  const reader = new FileReader();
+  reader.onload = function (e) {
+    logoDataURL = e.target.result;
+    const img = new Image();
+    img.onload = function () {
+      logoPreview.text("");
+      logoPreview.append(img);
+    };
+    img.src = logoDataURL;
+  };
+  reader.readAsDataURL(file);
 });
